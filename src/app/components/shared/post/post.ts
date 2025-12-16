f6fbe37ca9f0672c892ae0e3221df86f7b8528eb
kronhardtluis <kronhardtluis@gmail.com>
2025-12-16 13:31:52 +0100

    Erstellung des Übungsprojekts

---
+++ src/app/components/shared/post/post.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  imports: [],
+  templateUrl: './post.html',
+  styleUrl: './post.scss',
+})
+export class Post {
+
+}
+
+/**
+ * {
+            name: 'Hans',
+            image: 'assets/img/Bella 1.jpg',
+            likes: 55,
+            isLiked: true,
+            comments: ['Tolle banane', 'Banana in the Box'],
+        },
+        {
+            name: 'Franz',
+            image: 'assets/img/gatze.jpg',
+            likes: 21,
+            isLiked: false,
+            comments: [],
+        },
+        {
+            name: 'Mama',
+            image: 'assets/img/gatze7.PNG',
+            likes: 274,
+            isLiked: true,
+            comments: ['Schöne Orangen.'],
+        },
+         */
+
+        // Hauptseite erstellen
---
+++ src/app/interfaces/post.ts
@@ -0,0 +1,7 @@
+export interface Post {
+    name: string;
+    isLiked: boolean;
+    image: string;
+    likes: number;
+    comments: string[];
+}
